e5fed90445245f636c9db410fcfdce9de7b5c459
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-21 15:30:12 +0200

    Java, ejercicio 13. Completado

--- src/main/java/com/campusdual/classroom/Exercise13.java
+++ src/main/java/com/campusdual/classroom/Exercise13.java
@@ -3,9 +3,9 @@ package com.campusdual.classroom;
 public class Exercise13 {
     public static void main(String[] args) {
         FuelTank ft = new FuelTank();
-        ft.showDetails();
+        ft.setActualFuel(15);
         System.out.println("Actualización capacidad");
-        ft.actualFuel = -8;
+        ft.setActualFuel(-8);
         ft.showDetails();
     }
 }
